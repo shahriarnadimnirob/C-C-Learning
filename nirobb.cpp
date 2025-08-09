9f19a53533863284a0db8c1ea47f71637fb8a1e1
shahriarnadimnirob <nirobnkbd2007@gmail.com>
2025-08-09 14:11:49 +0600

    add some comment in this file

--- nirobb.cpp
+++ nirobb.cpp
@@ -1,4 +1,9 @@
 niro#include <stdio.h>
+
+/* nirobb.cpp
+ This file is part of the Niro project.
+ It is a simple C++ program that prints "Hello world" to the console.
+*/
 int main(){
     printf("Hello world\n");
     return 0;
